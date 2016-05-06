8a5709f75f54e303e626fb7d73e4db7878a2dc0a
dblanchard13 <dblanchard@gobalto.com>
2016-05-06 16:40:05 -0700

    Remove unnecessary config

--- index.js
+++ index.js
@@ -5,33 +5,9 @@ module.exports = (function(){
   var path = require('path');
   var join = path.join;
   var rootDir = path.resolve(join(__dirname, '../../'));
-  var packageJsonNode = 'brDirs';
-  var FileName = 'br'; 
-  var rootConfig;
-
-  function initConfig() {
-    try {
-      rootConfig = require(join(rootDir, FileName));
-    } catch (e) {
-        try {
-          rootConfig = require(join(rootDir, 'package.json'))[packageJsonNode];
-        } catch (e2) {}
-    }
-  };
-
-  initConfig();
-
-
-  function getFileName(fileNameOrKey, append) {
-    var configValue = rootConfig && rootConfig[fileNameOrKey];
-
-    return configValue ? join(configValue, append || '') : fileNameOrKey;
-  }
 
   return {
-    require: (fileName, append) => {
-      return require(join(rootDir, getFileName(fileName, append)));
-    }
+    require: (fileName, append) => require(join(rootDir, fileName))
   };
     
 }());
